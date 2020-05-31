faa9f6052899f9023edde9644d076687def16e60
Ethan <ethan@plexiate.com>
2020-05-30 21:32:37 -0400

    random amount added to stackableitems trigger

--- src/main/java/com/plexiate/triggers/StackableItemTrigger.java
+++ src/main/java/com/plexiate/triggers/StackableItemTrigger.java
@@ -2,12 +2,12 @@ package com.plexiate.triggers;
 
 import haveric.stackableItems.util.InventoryUtil;
 import net.Indyuce.mmocore.api.quest.trigger.Trigger;
+import net.Indyuce.mmocore.api.util.math.formula.RandomAmount;
 import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.Type;
 import net.mmogroup.mmolib.api.MMOLineConfig;
 import net.Indyuce.mmocore.api.player.PlayerData;
 import org.apache.commons.lang.Validate;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,7 +16,7 @@ public class StackableItemTrigger extends Trigger {
 
     private final String id;
 
-    private final int amount;
+    private final RandomAmount amount;
 
     public StackableItemTrigger(MMOLineConfig config) {
         super(config);
@@ -25,14 +25,14 @@ public class StackableItemTrigger extends Trigger {
         Validate.isTrue(MMOItems.plugin.getTypes().has(format), "Could not find item type " + format);
         this.type = MMOItems.plugin.getTypes().get(format);
         this.id = config.getString("id").replace("-", "_").toUpperCase();
-        this.amount = config.contains("amount") ? Math.max(1, config.getInt("amount")) : 1;
+        this.amount = new RandomAmount(config.getString("amount"));
         Validate.isTrue(this.type.getConfigFile().getConfig().contains(this.id), "Could not find item id " + this.id);
     }
     public void apply(PlayerData player) {
         ItemStack item = MMOItems.plugin.getItems().getItem(this.type, this.id);
         if (item == null || item.getType() == Material.AIR)
             return;
-        item.setAmount(this.amount);
+        item.setAmount(this.amount.calculateInt());
         if (item != null && item.getType() != Material.AIR)
             InventoryUtil.addItemsToPlayer(player.getPlayer(), item, "");
     }
